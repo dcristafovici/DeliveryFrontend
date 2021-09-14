e3f83b061eaa820b8bb33bfe5585ed44c77c1a05
Dan Cristafovici <Dan.Cristafovici@endava.com>
2021-09-14 06:56:21 +0300

    Renovation

--- src/Interfaces/RestaurantInterface.tsx
+++
@@ -1,9 +0,0 @@
-export interface RestaurantInterface {
-  id: string;
-  name: string;
-  description?: string;
-  minPrice: number;
-  saleValue: number;
-  deliveryTime: string;
-  image: string;
-}
--- src/Pages/RestaurantPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Restaurant from '../Components/Restaurant/Restaurant';
-
-const RestaurantPage:React.FC = () => (
-  <Restaurant />
-);
-
-export default RestaurantPage;
